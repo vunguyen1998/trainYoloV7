20d1b86e7e67fb3b4230641a2785eb817f393b70
vunguyen1998 <nguyensepi@gmail.com>
2023-06-22 09:43:13 +0700

    Update plot

--- utils/plots.py
+++ utils/plots.py
@@ -82,7 +82,7 @@ class Annotator:
             self.im = im
         self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
 
-    def box_label(self, box, label='', font_size=1, color=(128, 128, 128), txt_color=(255, 255, 255)):
+    def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
         # Add one xyxy box to image with label
         if self.pil or not is_ascii(label):
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
@@ -100,17 +100,19 @@ class Annotator:
             p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
             cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
             if label:
-                w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 5, thickness=font_size)[0]  # text width, height
+                tf = max(self.lw - 1, 1)  # font thickness
+                w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
                 outside = p1[1] - h >= 3
                 p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
                 cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
                 cv2.putText(self.im,
                             label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
                             0,
-                            self.lw / 5,
+                            self.lw / 3,
                             txt_color,
-                            thickness=font_size,
+                            thickness=tf,
                             lineType=cv2.LINE_AA)
+
 
     def draw_trk(self,thickness,centroids):
         [cv2.line(self.im, (int(centroids.centroids[i][0]),int(centroids.centroids[i][1])),
